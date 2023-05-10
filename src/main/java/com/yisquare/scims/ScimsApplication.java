58c45dd825661980c3c23ab0da49e4716b99aa12
Vicky <1526554553@qq.com>
2023-05-10 10:25:00 +0800

    第一次提交

---
+++ src/main/java/com/yisquare/scims/ScimsApplication.java
@@ -0,0 +1,13 @@
+package com.yisquare.scims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScimsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScimsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yisquare/scims/ScimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yisquare.scims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScimsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
